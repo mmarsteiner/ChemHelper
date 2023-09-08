5d739ecc3211d6f4567eafb463a122013676777d
Matt Marsteiner <31623939+mmarsteiner@users.noreply.github.com>
2023-09-07 22:34:56 -0400

    Sync & remove debugger printfs

--- src/main.cpp
+++ src/main.cpp
@@ -10,15 +10,8 @@
 
 int main(void)
 {
-    int *testNull = nullptr;
-    dbg_printf("This is what a null pointer looks like: %p\n", testNull);
-    dbg_printf("Pre initialization\n");
     Menu mainMenu("Chem Helper");
-    dbg_printf("Menu object created\n");
     mainMenu.addItem(new MolarMassModule);
-    dbg_printf("Module added to menu\n");
     mainMenu.start();
-    dbg_printf("Menu started\n");
-    /* Return 0 for success */
     return 0;
 }
